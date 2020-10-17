3b704cd4f9b60c944432c46c53fcb3f27e63d618
WillianFarias <williansousafarias@gmail.com>
2020-10-16 23:07:25 -0300

    model de produto

---
+++ src/models/Product.js
@@ -0,0 +1,25 @@
+//modelo mvc
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+//registro de product 
+mongoose.model('Product', ProductSchema);
