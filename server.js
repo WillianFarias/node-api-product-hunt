915329b99992b062cd3649d496722020b5b3f1f7
WillianFarias <williansousafarias@gmail.com>
2020-10-16 21:44:32 -0300

    adicionando mongoose e conectando DB

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongosse = require('mongoose');
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongosse.connect('mongodb://localhost:27017/nodeapi');
+
+//Primeira rota
 app.get('/', (req, res) => {
   res.send('Hello Rocketseat');
 });
