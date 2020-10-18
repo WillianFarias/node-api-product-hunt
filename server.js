6974b715dc6008ff9f3235dbef7459d0b3ddb79c
WillianFarias <williansousafarias@gmail.com>
2020-10-18 14:41:26 -0300

    tornando a api publica com cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //Iniciando o App
 const app = express();
 //informa que minha aplicacao deve receber dados json
 app.use(express.json());
+//é possível configura quem pode acessar e outras config de seguranca
+app.use(cors);
 
 //Iniciando o DB
 mongoose.connect( 'mongodb://localhost:27017/nodeapi', 
